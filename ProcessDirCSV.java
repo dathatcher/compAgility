4181e7cb4a33d8555403044161f9526df9433618
David A. Thatcher <david.austin.thatcher@gmail.com>
2021-02-01 00:52:10 +0000

    fix

--- ProcessDirCSV.java
+++ ProcessDirCSV.java
@@ -1,12 +1,22 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Vector;
 public class ProcessDirCSV {    public static void main(String args[]) throws IOException {
       //Creating a File object for directory
       File directoryPath = new File("/workspace/java/fax");
       //List of all files and directories
       String contents[] = directoryPath.list();
       //System.out.println("List of files and directories in the specified directory:");
+
+      Vector system = new Vector();
+      Vector category = new Vector();
+      Vector score = new Vector();
+      int sysCntr = 0;
+      int catCntr = 0;
+      int scoreCntr = 0;
+      
       for(int i=0; i<contents.length; i++) {
         // System.out.println(contents[i]);
          String filename = contents[i];
@@ -16,6 +26,10 @@ public class ProcessDirCSV {    public static void main(String args[]) throws IO
             sc.useDelimiter(",");   //sets the delimiter pattern 
             int firstOccurence=0;
             String scat=filename;
+            
+            system.add(sysCntr, filename);
+            sysCntr++;
+
             while (sc.hasNext())  //returns a boolean value  
                     {  
                       String s = sc.next();
@@ -23,21 +37,48 @@ public class ProcessDirCSV {    public static void main(String args[]) throws IO
                       if (s.startsWith("People -") || s.startsWith("Process -" ) || s.startsWith("Tech -" ) ){
                           firstOccurence++;
                          if (firstOccurence <= 21) { 
-                             scat = scat +"," + s;
+                             scat = scat +"," + s+"("+firstOccurence+") ";
+                             category.add(catCntr, s); 
+                             catCntr++;                                                        
                             // System.out.println("FILENAME: " + s);  //find and returns the next complete token from this scanner  
                           }
                        }
-                       if (firstOccurence >= 21 && firstOccurence < 26){
+                       if (firstOccurence >= 21 && firstOccurence < 45){
                            firstOccurence++;
-                           if (firstOccurence >= 22 && firstOccurence< 26){
-                               scat = scat +"," + s;
+                           if (firstOccurence > 23 && firstOccurence< 45){
+                               scat = scat +"," + s+"("+firstOccurence+") ";
+                               score.add(scoreCntr, s);
+                               scoreCntr++;                               
                            }
                        }
                         }
-                    System.out.println("FINAL: " + scat);    
+                    System.out.println("system: " + system.size());
+                    System.out.println("category: " + category.size());
+                    System.out.println("score: " + score.size()); 
+                    Iterator isystem = system.iterator();  
+                    System.out.println("The iterator values are: "); 
+                    while (isystem.hasNext()) { 
+                        System.out.println(isystem.next()); 
+                    } 
+                    Iterator icategory = category.iterator();  
+                    System.out.println("The iterator values are: "); 
+                    while (icategory.hasNext()) { 
+                        System.out.println(icategory.next()); 
+                    } 
+                    Iterator iscore = score.iterator();  
+                    System.out.println("The iterator values are: "); 
+                    while (iscore.hasNext()) { 
+                        System.out.println(iscore.next()); 
+                    } 
+                    system.clear();
+                    category.clear();
+                    score.clear();
+                    sysCntr = 0;
+                    catCntr = 0;
+                    scoreCntr = 0;
                     sc.close();  
          }
-         
+          
       }
    }
 }
